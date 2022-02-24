3997c612efc1bcd79fa5a9545320902cd492ead4
stovepipe <zach.mehlenbacher@gmail.com>
2022-02-23 22:21:22 -0500

    Flask route created

---
+++ app.py
@@ -0,0 +1,11 @@
+# Import dependencies
+from flask import Flask
+
+# Create flask instance
+app = Flask(__name__)
+
+# Create flask route
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
