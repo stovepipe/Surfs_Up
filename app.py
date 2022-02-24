04cc35e07ce018bb5c79791274262ec460b28913
stovepipe <zach.mehlenbacher@gmail.com>
2022-02-23 23:02:52 -0500

    Flask monthly temp route created

--- app.py
+++ app.py
@@ -56,3 +56,13 @@ def stations():
     stations = list(np.ravel(results))
     return jsonify(stations=stations)
 
+# Monthly temperature route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
